52bc7c076ff300724b90d6a5a0a58c918c0b9585
meltworks <76448415+meltworks@users.noreply.github.com>
2020-12-30 19:57:36 +0300

    Create returning-strings.sql

---
+++ kyu8/returning-strings.sql
@@ -0,0 +1,6 @@
+-- Returning Strings
+-- https://www.codewars.com/kata/55a70521798b14d4750000a4
+-- 30-12-2020
+
+SELECT CONCAT('Hello, ', name, ' how are you doing today?') AS greeting
+  FROM person
