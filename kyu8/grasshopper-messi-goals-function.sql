82ba31fb420d9aa9f657cdb2cb075f59d5bf25d1
meltworks <76448415+meltworks@users.noreply.github.com>
2020-12-31 20:19:10 +0300

    Create grasshopper-messi-goals-function.sql

---
+++ kyu8/grasshopper-messi-goals-function.sql
@@ -0,0 +1,6 @@
+-- Grasshopper - Messi goals function
+-- https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+-- 31-12-2020
+
+SELECT (la_liga_goals + copa_del_rey_goals + champions_league_goals) AS res
+  FROM goals
